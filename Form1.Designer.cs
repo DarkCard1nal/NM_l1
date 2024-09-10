482c7b03e71bc8f363e3ea0236f1a7065d599edd
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-09-10 19:12:37 +0300

    The user interface of the form was created, parsing of entered values was done, and a method for calculating the determinant of a 3x3 det3 matrix was written.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,97 +40,255 @@
 			this.textBox10 = new System.Windows.Forms.TextBox();
 			this.textBox11 = new System.Windows.Forms.TextBox();
 			this.textBox12 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(44, 54);
+			this.textBox1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(44, 35);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.Size = new System.Drawing.Size(100, 32);
 			this.textBox1.TabIndex = 0;
 			// 
 			// textBox2
 			// 
-			this.textBox2.Location = new System.Drawing.Point(230, 54);
+			this.textBox2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox2.Location = new System.Drawing.Point(227, 35);
 			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.Size = new System.Drawing.Size(100, 32);
 			this.textBox2.TabIndex = 0;
 			// 
 			// textBox3
 			// 
-			this.textBox3.Location = new System.Drawing.Point(415, 54);
+			this.textBox3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox3.Location = new System.Drawing.Point(412, 35);
 			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.Size = new System.Drawing.Size(100, 32);
 			this.textBox3.TabIndex = 0;
 			// 
 			// textBox4
 			// 
-			this.textBox4.Location = new System.Drawing.Point(44, 120);
+			this.textBox4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox4.Location = new System.Drawing.Point(44, 101);
 			this.textBox4.Name = "textBox4";
-			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.Size = new System.Drawing.Size(100, 32);
 			this.textBox4.TabIndex = 0;
 			// 
 			// textBox5
 			// 
-			this.textBox5.Location = new System.Drawing.Point(230, 120);
+			this.textBox5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox5.Location = new System.Drawing.Point(227, 101);
 			this.textBox5.Name = "textBox5";
-			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.Size = new System.Drawing.Size(100, 32);
 			this.textBox5.TabIndex = 0;
 			// 
 			// textBox6
 			// 
-			this.textBox6.Location = new System.Drawing.Point(415, 120);
+			this.textBox6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox6.Location = new System.Drawing.Point(412, 101);
 			this.textBox6.Name = "textBox6";
-			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.Size = new System.Drawing.Size(100, 32);
 			this.textBox6.TabIndex = 0;
 			// 
 			// textBox7
 			// 
-			this.textBox7.Location = new System.Drawing.Point(44, 198);
+			this.textBox7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox7.Location = new System.Drawing.Point(44, 179);
 			this.textBox7.Name = "textBox7";
-			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.Size = new System.Drawing.Size(100, 32);
 			this.textBox7.TabIndex = 0;
 			// 
 			// textBox8
 			// 
-			this.textBox8.Location = new System.Drawing.Point(230, 198);
+			this.textBox8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox8.Location = new System.Drawing.Point(227, 179);
 			this.textBox8.Name = "textBox8";
-			this.textBox8.Size = new System.Drawing.Size(100, 20);
+			this.textBox8.Size = new System.Drawing.Size(100, 32);
 			this.textBox8.TabIndex = 0;
 			// 
 			// textBox9
 			// 
-			this.textBox9.Location = new System.Drawing.Point(415, 198);
+			this.textBox9.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox9.Location = new System.Drawing.Point(412, 179);
 			this.textBox9.Name = "textBox9";
-			this.textBox9.Size = new System.Drawing.Size(100, 20);
+			this.textBox9.Size = new System.Drawing.Size(100, 32);
 			this.textBox9.TabIndex = 0;
 			// 
 			// textBox10
 			// 
-			this.textBox10.Location = new System.Drawing.Point(594, 54);
+			this.textBox10.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox10.Location = new System.Drawing.Point(591, 35);
 			this.textBox10.Name = "textBox10";
-			this.textBox10.Size = new System.Drawing.Size(100, 20);
+			this.textBox10.Size = new System.Drawing.Size(100, 32);
 			this.textBox10.TabIndex = 0;
 			// 
 			// textBox11
 			// 
-			this.textBox11.Location = new System.Drawing.Point(594, 120);
+			this.textBox11.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox11.Location = new System.Drawing.Point(591, 101);
 			this.textBox11.Name = "textBox11";
-			this.textBox11.Size = new System.Drawing.Size(100, 20);
+			this.textBox11.Size = new System.Drawing.Size(100, 32);
 			this.textBox11.TabIndex = 0;
 			// 
 			// textBox12
 			// 
-			this.textBox12.Location = new System.Drawing.Point(594, 198);
+			this.textBox12.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox12.Location = new System.Drawing.Point(591, 179);
 			this.textBox12.Name = "textBox12";
-			this.textBox12.Size = new System.Drawing.Size(100, 20);
+			this.textBox12.Size = new System.Drawing.Size(100, 32);
 			this.textBox12.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(113, 280);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(143, 46);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "СТАРТ";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.groupBox1.Location = new System.Drawing.Point(412, 243);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(279, 125);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Розв\'язок";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(18, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 72);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "x₁ = \r\nx₂ = \r\nx₃ = ";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(166, 42);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(43, 24);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "x₁ +";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(344, 42);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 24);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "x₂ +";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(527, 43);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(43, 24);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "x₃ =";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(166, 108);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(43, 24);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "x₁ +";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(344, 108);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(43, 24);
+			this.label6.TabIndex = 3;
+			this.label6.Text = "x₂ +";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.Location = new System.Drawing.Point(527, 109);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(43, 24);
+			this.label7.TabIndex = 3;
+			this.label7.Text = "x₃ =";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.Location = new System.Drawing.Point(166, 186);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(43, 24);
+			this.label8.TabIndex = 3;
+			this.label8.Text = "x₁ +";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label9.Location = new System.Drawing.Point(344, 186);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(43, 24);
+			this.label9.TabIndex = 3;
+			this.label9.Text = "x₂ +";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label10.Location = new System.Drawing.Point(527, 187);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(43, 24);
+			this.label10.TabIndex = 3;
+			this.label10.Text = "x₃ =";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 431);
+			this.ClientSize = new System.Drawing.Size(740, 402);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox12);
 			this.Controls.Add(this.textBox11);
 			this.Controls.Add(this.textBox10);
@@ -143,8 +301,13 @@
 			this.Controls.Add(this.textBox4);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
 			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -164,6 +327,18 @@
 		private System.Windows.Forms.TextBox textBox10;
 		private System.Windows.Forms.TextBox textBox11;
 		private System.Windows.Forms.TextBox textBox12;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
 
 namespace l1
 {
@@ -15,6 +18,94 @@ namespace l1
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		//Повертає детермінант матриці 3х3 методом трикутників.
+		private double det3(double[,] a)
+		{
+			return (a[0, 0] * a[1, 1] * a[2, 2] +
+					a[0, 1] * a[1, 2] * a[2, 0] +
+					a[0, 2] * a[1, 0] * a[2, 1] -
+					a[0, 2] * a[1, 1] * a[2, 0] -
+					a[0, 0] * a[1, 2] * a[2, 1] -
+					a[0, 1] * a[1, 0] * a[2, 2]);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			TextBox[] textBoxArray = new TextBox[12];
+			textBoxArray[0] = textBox1;
+			textBoxArray[1] = textBox2;
+			textBoxArray[2] = textBox3;
+			textBoxArray[3] = textBox4;
+			textBoxArray[4] = textBox5;
+			textBoxArray[5] = textBox6;
+			textBoxArray[6] = textBox7;
+			textBoxArray[7] = textBox8;
+			textBoxArray[8] = textBox9;
+			textBoxArray[9] = textBox10;
+			textBoxArray[10] = textBox11;
+			textBoxArray[11] = textBox12;
+
+			int i, j, x;
+			double detA, parsedValue;
+			double[,] a = new double[3, 3];
+			double[] b = new double[3];
+			double[] res = new double[3];
+			bool[] zero = new bool[6];
+
+			//Отримання значень з textBoxArray.
+			for (i = 0, x = 0; i < 3; i++)
+			{
+				for (j = 0; j < 3; j++, x++)
+				{
+					if (Double.TryParse(textBoxArray[x].Text, out parsedValue))
+					{
+						a[i, j] = parsedValue;
+					}
+					else
+					{
+						label1.Text = "Коренів не знайдено!";
+						return;
+					}
+				}
+			}
+			
+			for (i = 0; i < 3; i++)
+			{
+				if (Double.TryParse(textBoxArray[i + 9].Text, out parsedValue))
+				{
+					b[i] = parsedValue;
+				}
+				else
+				{
+					label1.Text = "Коренів не знайдено! Перевірте введення!";
+					return;
+				}
+			}
+
+			//Обчислення детермінанту матриці a
+			detA = det3(a);
+			if (detA == 0)
+			{
+				label1.Text = "Коренів не знайдено! Детермінант матриці 3х3 є 0!";
+			}
+
+			//
+
+
+			//Розв'язок СЛАР 3 методом Гаусса.
+
+
+
+
+
+
+
+
+			label1.Text = "x₁ = " + Convert.ToString(Math.Round(res[0], 4)) +
+				"\nx₂ = " + Convert.ToString(Math.Round(res[1], 4)) +
+				"\nx₃ = " + Convert.ToString(Math.Round(res[2], 4));
 		}
 	}
 }
